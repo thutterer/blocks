a7f962142767144807943ccefe25484dd6392282
Thomas Hutterer <thutterer@suse.de>
2017-02-23 10:48:43 +0100

    reset level and score display and game restart
    - also I don't like empty lines too much

--- tetris.js
+++ tetris.js
@@ -1,33 +1,28 @@
 //Golbal variables
-var ctx;        //Canvas object
- 
-var t;          //Tetrimino type
-var x, y;       //Tetrimino position
-var o;          //Tetrimino orientation
- 
-var grid;    //Game state grid
- 
-var timer;    //Game timer
- 
-var score;    //Player's score
-var level;    //Current level
-var timestep;    //Time between calls to gameStep()
- 
+var ctx;        // Canvas object
+var grid;       // Game state grid
+
+var t;          // Tetrimino type
+var x, y;       // Tetrimino position
+var o;          // Tetrimino orientation
+
+var timer;      // Game timer
+var timestep;   // Time between calls to gameStep()
+var score;      // Player's score
+var level;      // Current level
+
 /************************************************
 Initialize the drawing canvas
 ************************************************/
 function initialize() {
-  
     //Get the canvas context object from the body
     c = document.getElementById("myCanvas");
     ctx = c.getContext("2d");
-    
     //Initialize tetrimino variables
     t = 1 + Math.floor((Math.random()*7));
     x = 4;
     y = 18;
     o = 0;
-    
     //Create an empty game state grid
     grid = new Array(20);
     for(i = 0; i < 20; i++) {
@@ -35,39 +30,34 @@ function initialize() {
         for(j = 0; j < 10; j++)
             grid[i][j] = 0;
     }
-    
     //Draw the current tetrimino
     drawTetrimino(x,y,t,o,1);
-    
     //Redraw the grid
     drawGrid();
-    
     score = 0;
     level = 1;
+    updateScoreAndLevel();
     timestep = 1000;
-    
     //Start the game timer
     clearInterval(timer);
     timer = setInterval(function(){gameStep()}, timestep);
 }
- 
- 
+
 /************************************************
 Draws the current game state grid
 ************************************************/
 function drawGrid() {
-    
+
     //Clear the canvas
     ctx.clearRect(0,0,200,400);
-    
+
     //Loop over each grid cell
     for(i = 0; i < 20; i++) {
         for(j = 0; j < 10; j++)
             drawBlock(j, i, grid[i][j]);
     }
 }
- 
- 
+
 /************************************************
 Draws a block at the specified game coordinate
 x = [0,9]   x-coordinate
@@ -75,10 +65,10 @@ y = [0,19]  y-coordinate
 t = [0,7]   block type
 ************************************************/
 function drawBlock(x, y, t) {
-    
+
     //Check if a block needs to be drawn
     if(t > 0) {
-    
+
         //Get the block color
         var c;
         if(t == 1)        //I type
@@ -95,26 +85,26 @@ function drawBlock(x, y, t) {
             c = 280;    //Purple
         else            //Z type
             c = 0;        //Red
-    
+
         //Convert game coordinaes to pixel coordinates
         pixelX = x*20;
         pixelY = (19-y)*20;
-          
-          
+
+
         /**** Draw the center part of the block ****/
-          
+
         //Set the fill color using the supplied color
         ctx.fillStyle = "hsl(" + c + ",100%,50%)";
-          
+
         //Create a filled rectangle
         ctx.fillRect(pixelX+2,pixelY+2,16,16);
-        
-        
+
+
         /**** Draw the top part of the block ****/
-          
+
         //Set the fill color slightly lighter
         ctx.fillStyle = "hsl(" + c + ",100%,70%)";
-          
+
         //Create the top polygon and fill it
         ctx.beginPath();
         ctx.moveTo(pixelX,pixelY);
@@ -122,13 +112,13 @@ function drawBlock(x, y, t) {
         ctx.lineTo(pixelX+18,pixelY+2);
         ctx.lineTo(pixelX+2,pixelY+2);
         ctx.fill();
-          
-          
+
+
         /**** Draw the sides of the block ****/
-          
+
         //Set the fill color slightly darker
         ctx.fillStyle = "hsl(" + c + ",100%,40%)";
-          
+
         //Create the left polygon and fill it
         ctx.beginPath();
         ctx.moveTo(pixelX,pixelY);
@@ -136,7 +126,7 @@ function drawBlock(x, y, t) {
         ctx.lineTo(pixelX+2,pixelY+18);
         ctx.lineTo(pixelX+2,pixelY+2);
         ctx.fill();
-          
+
         //Create the right polygon and fill it
         ctx.beginPath();
         ctx.moveTo(pixelX+20,pixelY);
@@ -144,13 +134,13 @@ function drawBlock(x, y, t) {
         ctx.lineTo(pixelX+18,pixelY+18);
         ctx.lineTo(pixelX+18,pixelY+2);
         ctx.fill();
-          
-          
+
+
         /**** Draw the bottom part of the block ****/
-          
+
         //Set the fill color much darker
         ctx.fillStyle = "hsl(" + c + ",100%,30%)";
-          
+
         //Create the bottom polygon and fill it
         ctx.beginPath();
         ctx.moveTo(pixelX,pixelY+20);
@@ -160,8 +150,7 @@ function drawBlock(x, y, t) {
         ctx.fill();
     }
 }
-  
-  
+
 /*************************************************
 Draws a tetrimino at the specified game coordinate
 with the specified orientation
@@ -172,17 +161,17 @@ o = [0,3]   orientation
 d = [-1,1]    test, erase, or draw
 *************************************************/
 function drawTetrimino(x,y,t,o,d) {
- 
+
     //Determine the value to send to setGrid
     c = -1;
     if(d >= 0) c = t*d;
-    
+
     //Initialize validity test
     valid = true;
- 
+
     /**** Pick the appropriate tetrimino type ****/
     if(t == 1) { //I Type
-          
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y,c);
@@ -210,7 +199,7 @@ function drawTetrimino(x,y,t,o,d) {
         }
     }
     if(t == 2) { //J Type
-          
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y+1,c);
@@ -238,7 +227,7 @@ function drawTetrimino(x,y,t,o,d) {
         }
     }
     if(t == 3) { //L Type
-          
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y,c);
@@ -266,7 +255,7 @@ function drawTetrimino(x,y,t,o,d) {
         }
     }
     if(t == 4) { //O Type
-          
+
         //Orientation doesn’t matter
         valid = valid && setGrid(x,y,c);
         valid = valid && setGrid(x+1,y,c);
@@ -274,7 +263,7 @@ function drawTetrimino(x,y,t,o,d) {
         valid = valid && setGrid(x+1,y+1,c);
     }
     if(t == 5) { //S Type
-         
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y,c);
@@ -302,7 +291,7 @@ function drawTetrimino(x,y,t,o,d) {
         }
     }
     if(t == 6) { //T Type
-          
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y,c);
@@ -330,7 +319,7 @@ function drawTetrimino(x,y,t,o,d) {
         }
     }
     if(t == 7) { //Z Type
-          
+
         //Get orientation
         if(o == 0) {
             valid = valid && setGrid(x-1,y+1,c);
@@ -357,11 +346,10 @@ function drawTetrimino(x,y,t,o,d) {
             valid = valid && setGrid(x-1,y-1,c);
         }
     }
-    
+
     return valid;
 }
- 
- 
+
 /*************************************************
 Sets a grid cell in the game state grid
 x = [0,9]   x-coordinate
@@ -369,26 +357,25 @@ y = [0,19]  y-coordinate
 t = [-1,7]  test or block type
 *************************************************/
 function setGrid(x, y, t) {
-    
+
     //Check if point is in range
     if(x >= 0 && x < 10 && y >= 0 && y < 20) {
-        
+
         //Return test result if testing
         if(t < 0) return grid[y][x] == 0;
-        
+
         //Otherwise assign block type to the grid
         grid[y][x] = t;
         return true;
     }
     return false;
 }
- 
- 
+
 /*************************************************
 Responds to a key press event
 *************************************************/
 function keyDown(e) {
-    
+
     if(e.keyCode == 37) { //Left arrow
         drawTetrimino(x,y,t,o,0);  //Erase the current tetrimino
         x2 = x - 1;
@@ -415,48 +402,41 @@ function keyDown(e) {
     }
     else if(e.keyCode == 32) { //Space-bar
         drawTetrimino(x,y,t,o,0);  //Erase the current tetrimino
-        
+
         //Move down until invalid
         while(drawTetrimino(x,y-1,t,o,-1))
             y -= 1;
-            
+
         gameStep();
     }
-     
+
     //Draw the current tetrimino
     drawTetrimino(x,y,t,o,1);
-    
+
     //Redraw the grid
     drawGrid();
 }
- 
- 
+
 /*************************************************
 Updates the game state at regular intervals
 *************************************************/
 function gameStep() {
- 
     //Erase the current tetrimino
-    drawTetrimino(x,y,t,o,0);  
-    
+    drawTetrimino(x,y,t,o,0);
     //Check if the tetrimino can be dropped 1 block
     y2 = y - 1;
     if(drawTetrimino(x,y2,t,o,-1))
         y = y2;
     else {
-    
         //Redraw last tetrimino
         drawTetrimino(x,y,t,o,1);
-    
         //Check if any lines are complete
         checkLines();
-    
-        //Create a new tetrimino 
+        //Create a new tetrimino
         t2 = 1 + Math.floor((Math.random()*7));
         x2 = 4;
         y2 = 18;
         o2 = 0;
-        
         //Check if valid
         if(drawTetrimino(x2,y2,t2,o2,-1)) {
             t = t2;
@@ -469,62 +449,55 @@ function gameStep() {
             initialize();
             return;
         }
-        
     }
-    
     //Draw the current tetrimino
     drawTetrimino(x,y,t,o,1);
-    
     //Redraw the grid
     drawGrid();
 }
- 
- 
+
 /*************************************************
 Removes completed lines from the grid
 *************************************************/
 function checkLines() {
- 
-    //Loop over each line in the grid
+     //Loop over each line in the grid
     for(i = 0; i < 20; i++) {
-    
         //Check if the line is full
         full = true;
         for(j = 0; j < 10; j++)
             full = full && (grid[i][j] > 0);
-            
         if(full) {
-        
-            //Update score
+            //Increase score
             score++;
-            
             //Check if ready for the next level
             if(score >= level*10) {
                 level++;
-                
                 //Update the timer with a shorter timestep
                 timestep *= 0.8;
                 clearInterval(timer);
                 timer = setInterval(function(){gameStep()}, timestep);
             }
-        
             //Update score and level display
-            document.getElementById("score").innerHTML = "Level " + level + "   Score: " + score;
-        
+            updateScoreAndLevel();
             //Loop over the remaining lines
             for(ii = i; ii < 19; ii++) {
-                
                 //Copy each line from the line above
                 for(j = 0; j < 10; j++)
                     grid[ii][j] = grid[ii+1][j];
             }
-            
             //Make sure the top line is clear
             for(j = 0; j < 10; j++)
                 grid[19][j] = 0;
-                
             //Repeat the check for this line
             i--;
         }
     }
+}
+
+/*************************************************
+Updates the score and level
+*************************************************/
+function updateScoreAndLevel() {
+  document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("level").innerHTML = "Level: " + level;
 }
